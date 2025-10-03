095ca649bb798379682a32cbd78775ff9e4f9833
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-10-03 16:09:29 -0500

    bootstrapping in parallel and series

--- HW5_Braatz.R
+++ HW5_Braatz.R
@@ -152,11 +152,77 @@ class(all_list_csv)
 View(all_list_csv) #this is a list, not a dataframe 
 
 # Combine into one data frame
-all_data <- dplyr::bind_rows(all_list)
+all_data <- dplyr::bind_rows(all_list_csv) #wait, did we just remake what you gave us earlier?! Hmph! :/ 
+
+#####################################################################
+#Problem 5 --  Bootstrapping in series and parallel 
+####################################################################
+
+# Setup 
+
+library(parallel)                            #already built in, just call it 
+fish = all_data                              #renaming our dataframe 
+species_list = unique(fish$Species)               #list of species to loop over 
+library(dplyr)
+
+#creating a bootstrap function 
+
+boot_mean_weight = function(Species, n_boot, sample_size) {
+  #nboot = number of bootstraps 
+  #sample_size means sample 200 fish each time for each species 
+  x = fish %>% 
+  filter(Lake == "Erie", Species == !!Species) %>% 
+  pull(Weight_g) #it works like select but for a vector intsead of dataframe 
+  means = replicate(n_boot, mean(sample(x, size = sample_size, replace = TRUE)))
+  mean(means)
+}
+
+#testing bootstrap function 
+boot_mean_weight(species_list[3], 10000,200) #yep it's working 
+
+# Serial Option -----------------------------------------------------------
+
+t_serial <- system.time({                   # time the whole serial run
+  res_serial <- lapply(                     # loop over species in the main R process
+    species_list,                                # input: vector of species names
+    boot_mean_weight,                              # function to apply
+    n_boot = 10000,                           # number of bootstrap resamples per species
+    sample_size = 200                       # bootstrap sample size
+  )
+})
+
+t_serial
+# elapsed time = 1.69 seconds 
+
+# Parallel processing --------------------------------------------------------
+
+detectCores() #let's test how many cores my computer has, it says 14 but it probably multithreads, so it could be either 7 or 14-1 = 13 that we should use 
 
 
+n_cores <- max(1, detectCores() - 1)   # use all but one core (be nice to your laptop)
+n_cores = max(1, detectCores() / 2)# actually just using half our cores is better 
+cl <- makeCluster(n_cores)                  # start worker processes
+
+clusterSetRNGStream(cl, iseed = 123)        # make random numbers reproducible across workers
+
+# Send needed objects to workers (function + data + species vector)
+clusterEvalQ(cl, library(dplyr)) #apparently we need to load the dplyr library into the cluster evaluation or it gets real mad 
+clusterExport(cl, varlist = c("fish", "boot_mean_weight", "species"), envir = environment())
+# library("dplyr") originally I just tried calling hte dplyr library separately but it got confused 
+t_parallel <- system.time({                 # time the parallel run
+  res_parallel <- parLapply(                # same API as lapply(), but across workers
+    cl,                                     # the cluster
+    species,                                # each worker gets one species (or more)
+    boot_mean_weight,                              # function to run
+    n_boot = 10000,                           # same bootstrap settings as serial
+    sample_size = 200
+  )
+})
+stopCluster(cl)                             # always stop the cluster when done
+
+t_parallel
+#our elapsed time is 0.64, much smaller 
+#Interestingly, I ran it with n-1 cores = 13, but running it with 7 cores was even faster (0.59 seconds), probably due to hyperthreading. 
 
 
-
-
-
+# Overall: the series was slowest, running for 1.69 seconds. The parallel processing using all but one core was second fastest at 0.64 seconds. The parallel process using half hte cores was the fastest at 0.59 seconds, likely due to hyperthreading (it can process two cores from every one of its physical cores, but they might be sharing some resources and it could be easiest to just use your physical cores) 
